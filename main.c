45466b750f6deff36a28ec56eabef317b5f9f53c
Onildo R Ribeiro <oricardoribeiro.sa@gmail.com>
2021-03-16 06:03:24 -0300

    Upload main.c

--- main.c
+++ main.c
@@ -1,44 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Redefinindo os tipos básicos float e int
-
-typedef float nota;
-typedef int inteiro;
-
-struct tAluno
-{
-// Utilizando os tipos redefinidos dentro da struct
-inteiro matricula;
-nota prova1;
-nota prova2;
-};
-
-// Redefinindo a struct para encurtar o comando na declaração
-
-typedef struct tAluno tAluno;
-
 int main(void)
 {
-// Declarando a variável do tipo tAluno: note que a redefinição
-// agora não é mais necessário escrever struct tAluno
-	tAluno aluno;
+	struct coisas
+	{
+		int cod_thing;
+		char thing[];
+	};
 
-	nota media = 0;
+	struct coisas t;
 
-	printf("\n\nInforme o numero da matrícula: ");
-	scanf("%d", &aluno.matricula);
+	system("reset");
 
-	printf("Informe a nota da primeira prova: ");
-	scanf("%f", &aluno.prova1);
-
-	printf("Informe a nota da segunda prova: ");
-	scanf("%f", &aluno.prova2);
-
-	media = (aluno.prova1 + aluno.prova2) / 2;
-
-	printf("\nMatrícula.....: %d\n", aluno.matricula);
-	printf("Media do aluno: %.2f\n\n", media);
+	printf("Qual o código da coisa: ");
+	scanf("%d", &t.cod_thing);
 
 	return(0);
 }
